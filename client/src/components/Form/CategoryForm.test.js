0d3d4b9673d17019e4f298a1536fef3667fc1229
jonasongg <jonasong2@gmail.com>
2026-02-13 20:35:56 +0800

    add tests for CategoryForm

--- client/src/components/Form/CategoryForm.test.js
+++ client/src/components/Form/CategoryForm.test.js
@@ -3,6 +3,41 @@ import CategoryForm from "./CategoryForm";
 import React from "react";
 
 describe("Category Form Component", () => {
+  it("should render the form correctly", () => {
+    const handleSubmit = jest.fn();
+    const setValue = jest.fn();
+    const value = "";
+
+    render(
+      <CategoryForm
+        handleSubmit={handleSubmit}
+        value={value}
+        setValue={setValue}
+      />,
+    );
+
+    expect(
+      screen.getByPlaceholderText("Enter new category"),
+    ).toBeInTheDocument();
+    expect(screen.getByText("Submit")).toBeInTheDocument();
+  });
+
+  it("should initialise with empty input", () => {
+    const handleSubmit = jest.fn();
+    const setValue = jest.fn();
+    const value = "";
+
+    render(
+      <CategoryForm
+        handleSubmit={handleSubmit}
+        value={value}
+        setValue={setValue}
+      />,
+    );
+
+    expect(screen.getByPlaceholderText("Enter new category").value).toBe("");
+  });
+
   it("should handle form submission correctly", async () => {
     const handleSubmit = jest.fn((e) => e.preventDefault());
     const setValue = jest.fn();
